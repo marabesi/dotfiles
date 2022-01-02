9d8b9fbe4c8db06ec6caa6fd9f9753a51e0f75c0
Matheus Marabesi <matheus.marabesi@gmail.com>
2022-01-02 19:46:03 +0100

    Create apps.sh

---
+++ apps.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+
+## after install, upgrade
+
+sudo apt update -y && sudo apt-get upgrade -y
+
+
+sudo apt install -y apt-transport-https curl
+
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+
+sudo apt update -y
+
+sudo apt install -y brave-browser
+
+## vim
+
+sudo apt-get install -y neovim
+
+## ulauncher
+
+sudo add-apt-repository -y ppa:agornostal/ulauncher && sudo apt update -y && sudo apt install -y ulauncher
+
+## oh myzsh
+
+sudo apt-get install -y zsh && sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+## vscode
+
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+
+sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+
+rm -f packages.microsoft.gpg
+
+sudo apt update -y
+
+sudo apt install -y code # or code-insiders
+
+## obs studio
+
+sudo add-apt-repository -y ppa:obsproject/obs-studio
+sudo apt update -y
+sudo apt install -y obs-studio
+
+
+## docker
+
+ sudo apt-get update -y
+ sudo apt-get install -y \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+ echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+ sudo apt-get update -y
+ sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+ ## docker compose
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+
+ # refs https://www.digitalocean.com/community/questions/how-to-fix-docker-got-permission-denied-while-trying-to-connect-to-the-docker-daemon-socket
+sudo usermod -aG docker ${USER}
+
+## spotify
+
+curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | sudo apt-key add - 
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt-get update -y && sudo apt-get install -y spotify-client
