76e410fb482c4a1ae51e83f44a83c41174b93f66
Matheus Marabesi <matheus.marabesi@gmail.com>
2022-01-06 11:25:10 +0100

    chore: adds dropbox

--- apps.sh
+++ apps.sh
@@ -130,3 +130,11 @@ sudo apt-get install surfshark-vpn -y
 
 curl -s "https://get.sdkman.io" | bash
 source "$HOME/.sdkman/bin/sdkman-init.sh"
+
+
+## dropbox
+
+wget https://www.dropbox.com/download\?dl\=packages/ubuntu/dropbox_2020.03.04_amd64.deb
+(sudo dpkg -i download\?dl=packages%2Fubuntu%2Fdropbox_2020.03.04_amd64.deb || true)
+sudo apt install -f -y
+rm download\?dl=packages%2Fubuntu%2Fdropbox_2020.03.04_amd64.deb
